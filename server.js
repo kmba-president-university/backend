031b43c34f9d406b1151511ffd5eb09659bd8636
johe lappy <johevin.blesstowi@binus.ac.id>
2024-07-21 21:14:24 +0700

    debug connection backend

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "dev_server",
-  user: "root",
-  password: "satuperxdx",
-  database: "db_signup",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 // Route to check the database connection
 app.get("/check-connection", (req, res) => {
   db.connect((err) => {
-    if (err) {
+    if (err) { 
       console.error("Error connecting to the database:", err);
-      return res.status(500).json({
-        status: "Error",
-        message: "Failed to connect to the database",
-        error: err,
-      });
+      return res 
+        .status(500)
+        .json({
+          status: "Error",
+          message: "Failed to connect to the database",
+          error: err,
+        });
     }
     console.log("Connected to the database");
     return res
@@ -42,6 +46,7 @@ app.post("/signup", (req, res) => {
   });
 });
 
-app.listen(8081, () => {
-  console.log("Server is listening on port 8081");
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
