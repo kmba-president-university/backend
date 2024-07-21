99346a780d8ce11ae77f5f3c5e53af7dda609ae0
johe lappy <johevin.blesstowi@binus.ac.id>
2024-07-21 21:25:33 +0700

    debug connection backend

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10, // Adjust as needed
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -17,18 +19,17 @@ const db = mysql.createConnection({
 
 // Route to check the database connection
 app.get("/check-connection", (req, res) => {
-  db.connect((err) => {
-    if (err) { 
+  pool.getConnection((err, connection) => {
+    if (err) {
       console.error("Error connecting to the database:", err);
-      return res 
-        .status(500)
-        .json({
-          status: "Error",
-          message: "Failed to connect to the database",
-          error: err,
-        });
+      return res.status(500).json({
+        status: "Error",
+        message: "Failed to connect to the database",
+        error: err,
+      });
     }
     console.log("Connected to the database");
+    connection.release(); // Release connection back to the pool
     return res
       .status(200)
       .json({ status: "Success", message: "Connected to the database" });
@@ -38,9 +39,17 @@ app.get("/check-connection", (req, res) => {
 app.post("/signup", (req, res) => {
   const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES (?)";
   const values = [req.body.name, req.body.email, req.body.password];
-  db.query(sql, [values], (err, data) => {
+
+  pool.query(sql, [values], (err, data) => {
     if (err) {
-      return res.json("Error");
+      console.error("Error executing query:", err);
+      return res
+        .status(500)
+        .json({
+          status: "Error",
+          message: "Error executing query",
+          error: err,
+        });
     }
     return res.json(data);
   });
